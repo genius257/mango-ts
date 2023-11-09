81898a018a5251ebe32337de6b1ae5682d27bf36
Anders Pedersen <hot415@hotmail.com>
2023-11-09 23:17:06 +0000

    Fixed issue with wrong return type on function signatures

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+declare const brand: unique symbol;
+type Opaque<T, K> = K & {readonly [brand]: T}
+
 export type Manga = {
     id: string;
     title: string;
@@ -25,11 +28,16 @@ export type Page = {
     headers: Record<string, string>;
 };
 
-export type searchManga = (query: string) => Array<Manga>;
-export type listChapters = (manga_id: string) => Array<Chapter>;
-export type selectChapter = (id: string) => Chapter;
-export type nextPage = () => Page;
+export type searchManga = (query: string) => json_encoded<Array<Manga>>;
+export type listChapters = (manga_id: string) => json_encoded<Array<Chapter>>;
+export type selectChapter = (id: string) => json_encoded<Chapter>;
+export type nextPage = () => json_encoded<Page>;
 export type newChapters = (
     manga_id: string,
     after_timestamp: number
-) => Array<Chapter>;
+) => json_encoded<Array<Chapter>>;
+
+type json_encoded<T> = Opaque<T, "JSON">;
+export function json_encode<T>(value: T): json_encoded<T> {
+    return JSON.stringify(value) as json_encoded<T>;
+}
