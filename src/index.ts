6ed3ce3ca22edf6be0a41089c8a1a5b7fb44db4a
Anders Pedersen <hot415@hotmail.com>
2023-11-11 21:44:29 +0000

    Moved global const declaration mango into main index ts file

--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,38 @@ type json_encoded<T> = Opaque<T, "JSON">;
 export function json_encode<T>(value: T): json_encoded<T> {
     return JSON.stringify(value) as json_encoded<T>;
 }
+
+type MangoHttpResult = {
+    status_code: number;
+    body: string;
+    headers: Record<string, string>;
+}
+
+type MangoStatic = {
+    get(url: string): MangoHttpResult;
+    get(url: string, headers: Record<string, string>): MangoHttpResult;
+
+    post(url: string, body: string): MangoHttpResult;
+    post(
+        url: string,
+        body: string,
+        headers: Record<string, string>
+    ): MangoHttpResult;
+
+    css(html: string, selector: string): string[];
+
+    text(html: string): string;
+
+    attribute(html: string, attr: string): string;
+
+    storage(key: string): string | undefined;
+    storage(key: string, val: unknown): unknown;
+
+    settings(key: string): string | null | undefined;
+
+    raise(msg: string): unknown;
+}
+
+declare const mango: MangoStatic;
+
+export default mango;
