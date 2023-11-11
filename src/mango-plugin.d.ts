8561fe6f986ac3fcabb13a04d4aa0d79b32381f5
Anders Pedersen <hot415@hotmail.com>
2023-11-11 15:43:32 +0000

    Changed definition file in an attempt to fix global object availability

--- src/mango-plugin.d.ts
+++ src/mango-plugin.d.ts
@@ -1,53 +1,34 @@
-export { }
-
-declare global {
-    // Definition or type for the function.
-    type Debug = (label: string) => (message: any, ...args: any[]) => void
-
-    namespace Mango {
-        type HttpResult = {
-            status_code: number;
-            body: string;
-            headers: Record<string, string>;
-        };
-
-        interface MangoStatic {
-            get(url: string): HttpResult;
-            get(url: string, headers: Record<string, string>): HttpResult;
-
-            post(url: string, body: string): HttpResult;
-            post(
-                url: string,
-                body: string,
-                headers: Record<string, string>
-            ): HttpResult;
-
-            css(html: string, selector: string): string[];
-
-            text(html: string): string;
-
-            attribute(html: string, attr: string): string;
-
-            storage(key: string): string | undefined;
-            storage(key: string, val: unknown): unknown;
-
-            settings(key: string): string | null | undefined;
-
-            raise(msg: string): unknown;
-        }
-    }
-
-    // If defining an object you might do something like this
-    // interface IConfig { a: number, b: number }
-
-    // Extend the Global interface for the NodeJS namespace.
-    interface Window {
-        // Reference our above type, 
-        // this allows global.debug to be used anywhere in our code.
-        mango: Mango.MangoStatic
-    }
-
-    // This allows us to simply call debug('some_label')('some debug message')
-    // from anywhere in our code.
-    const mango: Mango.MangoStatic
+type MangoHttpResult = {
+    status_code: number;
+    body: string;
+    headers: Record<string, string>;
 }
+
+type MangoStatic = {
+    get(url: string): MangoHttpResult;
+    get(url: string, headers: Record<string, string>): MangoHttpResult;
+
+    post(url: string, body: string): MangoHttpResult;
+    post(
+        url: string,
+        body: string,
+        headers: Record<string, string>
+    ): MangoHttpResult;
+
+    css(html: string, selector: string): string[];
+
+    text(html: string): string;
+
+    attribute(html: string, attr: string): string;
+
+    storage(key: string): string | undefined;
+    storage(key: string, val: unknown): unknown;
+
+    settings(key: string): string | null | undefined;
+
+    raise(msg: string): unknown;
+}
+
+declare const mango: MangoStatic;
+
+export = mango;
